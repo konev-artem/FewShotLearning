afa1703d0dc4a12ad4e4f92569536edfabddb925
Kseniya Valchuk <ksenia.valchuk@gmail.com>
2019-03-20 15:37:36 +0300

    to cv2

--- data_provider/transform.py
+++ data_provider/transform.py
@@ -2,14 +2,14 @@ import cv2
 import tensorflow as tf
 
 class Augmentation:
-    def crop(self, img, value, size, seed=None, name=None):
-        return tf.image.random_crop(img, value, size, seed, name)
+    def crop(self, img, left, lower, h, w):
+        return img[lower:lower+h, left:left+w]
 
     def flip(self, img, direction='horizontal'):
         if direction == 'horizontal':
-            return tf.image.flip_left_right(img)
+            return cv2.flip(img, 0)
         if direction == 'vertical':
-            return tf.image.flip_up_down(img)
+            return cv2.flip(img, 1)
         raise ValueError('direction for flip can be horizontal or vertical')
 
     def color_jitter(self, img, flag=cv2.COLOR_BGR2RGB):
