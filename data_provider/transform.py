c7b5ed0dd3c86f6ce7fe3a4e951ccad537dab4f2
Kseniya Valchuk <ksenia.valchuk@gmail.com>
2019-03-29 20:18:52 +0300

    added apply transforms

--- data_provider/transform.py
+++ data_provider/transform.py
@@ -1,62 +1,85 @@
 import cv2
 import numpy as np
+import random
 
-class Augmentation:
-    def get_random_transform(self, img):
-        pass
+def apply_random_classic_transform(img, p=0.5):
+    transforms = [random_flip, random_crop, random_color_jitter]
+    for f in transforms: 
+        if random.random() < p: 
+            img = f(img)
+    return img
 
-    def apply_transform(self, img, transform='flip'):
-        return {
-            'flip': self.flip,
-            'color_jitter': self.color_jitter,
-            'crop': self.crop
-        }[transform](img)
+def apply_mixed_transform(img1, img2, y1, y2, p=0.5):
+    transforms = [mixup, noisy_mixup, between_class, vertical_concat, horizontal_concat, mixed_concat]
+    for f in transforms: 
+        if random.random() < p: 
+            img = f(img1, img2, y1, y2)
+    return img
 
-    def crop(self, img, left, lower, h, w):
-        return img[lower:lower+h, left:left+w]
+def crop(img, left, lower, h, w):
+    return img[lower:lower+h, left:left+w]
 
-    def flip(self, img, direction='horizontal'):
-        if direction == 'horizontal':
-            return cv2.flip(img, 0)
-        if direction == 'vertical':
-            return cv2.flip(img, 1)
-        raise ValueError('direction for flip can be horizontal or vertical')
+def random_crop(img):
+    left = np.random.choice(img.shape[1])
+    lower = np.random.choice(img.shape[0])
+    w = np.random.choice(img.shape[1] - left)
+    h = np.random.choice(img.shape[0] - lower)
+    return crop(img, left, lower, h, w)
 
-    def color_jitter(self, img, flag=cv2.COLOR_BGR2RGB):
-        return cv2.cvtColor(img, flag)
+def flip(img, direction='horizontal'):
+    if direction == 'horizontal':
+        return cv2.flip(img, 0)
+    if direction == 'vertical':
+        return cv2.flip(img, 1)
+    raise ValueError('direction for flip can be horizontal or vertical')
 
-    def mixup(self, img1, img2, y1, y2, alpha=1):
-        coeff = np.random.beta(alpha, alpha)
-        img = coeff * img1 + (1 - coeff) * img2
-        y = coeff * y1 + (1 - coeff) * y2
-        return (img, y)
+def random_flip(img):
+    direction = np.random.choice(2)
+    return cv2.flip(img, direction)
     
-    def noisy_mixup(self, img1, img2, y1, y2, alpha=1, scale=0.025):
-        coeff = np.random.beta(alpha, alpha)
-        y = coeff * y1 + (1 - coeff) * y2
-        coeff += np.random.normal(scale=scale, size=(150, 150))
-        constrain = lambda x : max(min(x, 1), 0)
-        coeff = np.vectorize(constrain)(coeff)
-        coeff = coeff[:, :, None]
-        img = coeff * img1 + (1 - coeff) * img2
-        return (img, y)
+def color_jitter(img, flag=cv2.COLOR_BGR2RGB):
+    return cv2.cvtColor(img, flag)
 
-    def between_class(self, img1, img2, y1, y2, coeff=1):
-        pass
-    
-    def vertical_concat(self, img1, img2, y1, y2, alpha=1):
-        coeff = np.random.beta(alpha, alpha)
-        y = coeff * y1 + (1 - coeff) * y2
-        upper = img1[:int(coeff * img1.shape[0])]
-        lower = img2[int(coeff * img2.shape[0]):]
-        return (np.concatenate((upper, lower)), y)
-    
-    def horizontal_concat(self, img1, img2, y1, y2, alpha=1):
-        coeff = np.random.beta(alpha, alpha)
-        y = coeff * y1 + (1 - coeff) * y2
-        left = img1[:, :int(coeff * img1.shape[1])]
-        right = img2[:, int(coeff * img2.shape[1]):]
-        return (np.concatenate((left, right), axis=1), y)
-    
-    def mixed_concat(self, img1, img2, y1, y2, coeff=1):
-        pass
+def random_color_jitter(img):
+    flags = [i for i in dir(cv2) if i.startswith('COLOR_')]
+    flag = np.random.choice(flags)
+    return color_jitter(img, getattr(cv2, flag))
+
+def mixup(img1, img2, y1, y2, alpha=1):
+    coeff = np.random.beta(alpha, alpha)
+    img = coeff * img1 + (1 - coeff) * img2
+    y = coeff * y1 + (1 - coeff) * y2
+    return (img, y)
+
+def noisy_mixup(img1, img2, y1, y2, alpha=1, scale=0.025):
+    coeff = np.random.beta(alpha, alpha)
+    y = coeff * y1 + (1 - coeff) * y2
+    coeff += np.random.normal(scale=scale, size=(150, 150))
+    constrain = lambda x : max(min(x, 1), 0)
+    coeff = np.vectorize(constrain)(coeff)
+    coeff = coeff[:, :, None]
+    img = coeff * img1 + (1 - coeff) * img2
+    return (img, y)
+
+def between_class(img1, img2, y1, y2, coeff=np.random.uniform(0, 1)):
+    sigma1 = img1.std()
+    sigma2 = img2.std()
+    p = 1 / (1 + sigma1 / sigma2 * (1 - coeff) / coeff)
+    return (p * (img1 - img1.mean()) + (1 - p) * (img2 - img2.mean())) / (p**2 + (1 - p)**2)
+
+def vertical_concat(img1, img2, y1, y2, alpha=1):
+    coeff = np.random.beta(alpha, alpha)
+    y = coeff * y1 + (1 - coeff) * y2
+    upper = img1[:int(coeff * img1.shape[0])]
+    lower = img2[int(coeff * img2.shape[0]):]
+    return (np.concatenate((upper, lower)), y)
+
+def horizontal_concat(img1, img2, y1, y2, alpha=1):
+    coeff = np.random.beta(alpha, alpha)
+    y = coeff * y1 + (1 - coeff) * y2
+    left = img1[:, :int(coeff * img1.shape[1])]
+    right = img2[:, int(coeff * img2.shape[1]):]
+    return (np.concatenate((left, right), axis=1), y)
+
+def mixed_concat(img1, img2, y1, y2, coeff=1):
+    pass
