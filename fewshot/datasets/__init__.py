06e5dfb5a42170b333ec322df6609ee47885dfc8
Dmitry Ischenko <ischenko.dmitry@gmail.com>
2019-04-01 20:57:47 +0300

    Initial project structure
    * Moved `datasets` to `fewshot` package
    * Declared all datasets preparers in __init__.py

---
+++ fewshot/datasets/__init__.py
@@ -0,0 +1,6 @@
+from .prepare_dataset import CUBPreparer, DatasetPreparer, EMNISTPreparer
+from .prepare_dataset import MiniImageNetPreparer, OmniglotPreparer
+
+
+__all__ = ["CUBPreparer", "DatasetPreparer", "EMNISTPreparer",
+           "MiniImageNetPreparer", "OmniglotPreparer"]
