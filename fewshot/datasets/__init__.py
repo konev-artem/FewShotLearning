74e6ed21c50a6cdfd1429f4731ef4300fc0c8d3f
Dmitry Ischenko <ischenko.dmitry@gmail.com>
2019-04-03 22:11:12 +0300

    Moved dataset preparers to scripts

--- fewshot/datasets/__init__.py
+++
@@ -1,6 +0,0 @@
-from .prepare_dataset import CUBPreparer, DatasetPreparer, EMNISTPreparer
-from .prepare_dataset import MiniImageNetPreparer, OmniglotPreparer
-
-
-__all__ = ["CUBPreparer", "DatasetPreparer", "EMNISTPreparer",
-           "MiniImageNetPreparer", "OmniglotPreparer"]
