2b490d4a634b165117f0e974381dfec23308bd2a
Dmitry Ischenko <ischenko.dmitry@gmail.com>
2019-04-17 01:28:44 +0300

    Implemented modules init in example script. Parse arguments in example script

--- fewshot/experiment/simple_experiment.py
+++ fewshot/experiment/simple_experiment.py
@@ -1,58 +1,36 @@
 from .experiment import Experiment, Report
-from ..backbones import ConvNet
+from ..backbones import ConvNet, Resnet12
 from ..data_provider import Dataset
 
-
-# just for example
-
-
-class BackboneTrainer:
-    def train(self, backbone_model, dataset):
-        ...
-
-
-class FewShotModel:
-    def __init__(self, backbone_model):
-        ...
-
-
-class Trainer:
-    def train(self, fewshot_model, dataset):
-        ...
-
-
-class Tester:
-    def test(self, fewshot_model):
-        # do some stuff
-        return 0
+from enum import Enum
 
 
 class SimpleExperiment(Experiment):
     """Basic experiment with metric learning approach"""
 
+    class BackboneType(str, Enum):
+        CONVNET = "convnet"
+        RESNET12 = "resnet12"
+
     def __init__(self, config):
         self.config = config
 
-        self.dataset = None
+        self.dataset = Dataset(config["dataset"]["dataset_dir"])
 
-        self.backbone_model = None
-        self.fewshot_model = FewShotModel(self.backbone_model)
+        width = height = config["backbone"]["input_size"]
+        input_size = (width, height, 3)
 
-        self.backbone_trainer = BackboneTrainer()
-        self.fewshot_trainer = Trainer()
-        self.tester = Tester()
+        if config["backbone"]["type"] == self.BackboneType.CONVNET:
+            self.backbone = ConvNet(input_size=input_size)
+        elif config["backbone"]["type"] == self.BackboneType.RESNET12:
+            self.backbone = Resnet12(input_size=input_size)
+
+    @classmethod
+    def get_availible_backbone_types(cls):
+        return [backbone_type.value for backbone_type in cls.BackboneType]
 
     def run(self):
-
-        # train backbone
-        self.backbone_trainer.train(self.backbone_model, self.dataset)
-
-        # train fewshot
-        self.fewshot_trainer.train(self.fewshot_model, self.dataset)
-
-        # test
-        metric = self.tester.test(self.fewshot_model)
-
+        metric = 0
         # create report
         report = Report(metric=metric)
         return report
--- scripts/example_simple.py
+++ scripts/example_simple.py
@@ -1,14 +1,37 @@
 from fewshot.experiment import SimpleExperiment
 
 import argparse
+import os
 
 
-# ... parse arguments ...
+# parse arguments
+parser = argparse.ArgumentParser(description='Baseline Few Shot Experiment')
+parser.add_argument('--dataset_dir', type=str, required=True,
+                    help="Path to dataset")
+parser.add_argument('--backbone_type', type=str, required=True,
+                    help="Type of backbone network",
+                    choices=SimpleExperiment.get_availible_backbone_types())
+parser.add_argument('--backbone_input_size', type=int, default=64,
+                    help="Width and height of backbone first layer input"
+                         "Images will be rescaled to this size")
+args = parser.parse_args()
+
+
+# check dataset existance
+if not os.path.exists(args.dataset_dir):
+    print("Use prepare_dataset.py script to download dataset.")
+    exit()
+
+
+# prepare config
 config = {
     "dataset": {
-        "datase_dir": "./data/omniglot",
+        "dataset_dir": args.dataset_dir,
     },
-    "backbone": {},
+    "backbone": {
+        "type": args.backbone_type,
+        "input_size": args.backbone_input_size,
+    },
     "trainer": {},
     "tester": {},
     "metrics": {}
