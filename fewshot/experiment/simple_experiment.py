58ae964198120221dda7bb90f2541ee79249c46f
Dmitry Ischenko <ischenko.dmitry@gmail.com>
2019-04-17 02:26:08 +0300

    Dataset splits

--- fewshot/experiment/simple_experiment.py
+++ fewshot/experiment/simple_experiment.py
@@ -15,8 +15,20 @@ class SimpleExperiment(Experiment):
     def __init__(self, config):
         self.config = config
 
+        # prepare datasets
         self.dataset = Dataset(config["dataset"]["dataset_dir"])
 
+        # FIXME: actually not proper way to subset backbone dataset by putting
+        # it to test position
+        self.fewshot_dataset, self.backbone_dataset = self.dataset.split_by_classes(
+            test_size=config["dataset"]["backbone_dataset_size"]
+        )
+
+        self.backbone_train_dataset, self.backbone_val_dataset = (
+            self.backbone_dataset.split_by_objects(
+                test_size=config["dataset"]["backbone_val_size"]))
+
+        # prepare backbone
         width = height = config["backbone"]["input_size"]
         input_size = (width, height, 3)
 
@@ -30,7 +42,12 @@ class SimpleExperiment(Experiment):
         return [backbone_type.value for backbone_type in cls.BackboneType]
 
     def run(self):
-        metric = 0
+        # .. train backbone ...
+
+        # .. train fewshot ...
+
+        # .. test fewshot ...
+
         # create report
-        report = Report(metric=metric)
+        report = Report(metric=0.0)
         return report
--- scripts/example_simple.py
+++ scripts/example_simple.py
@@ -8,12 +8,19 @@ import os
 parser = argparse.ArgumentParser(description='Baseline Few Shot Experiment')
 parser.add_argument('--dataset_dir', type=str, required=True,
                     help="Path to dataset")
-parser.add_argument('--backbone_type', type=str, required=True,
+parser.add_argument('--backbone_type', type=str, default=SimpleExperiment.BackboneType.CONVNET,
                     help="Type of backbone network",
                     choices=SimpleExperiment.get_availible_backbone_types())
 parser.add_argument('--backbone_input_size', type=int, default=64,
                     help="Width and height of backbone first layer input"
                          "Images will be rescaled to this size")
+parser.add_argument('--backbone_dataset_size', type=float, default=0.5,
+                    help="Ratio of classes passed to backbone training")
+parser.add_argument('--backbone_val_size', type=float, default=0.1,
+                    help="Ratio of backbone dataset passed to validation for early stopping")
+parser.add_argument('--backbone_n_epochs', type=int, default=10,
+                    help="Number of epoch to train the backbone network")
+
 args = parser.parse_args()
 
 
@@ -27,12 +34,19 @@ if not os.path.exists(args.dataset_dir):
 config = {
     "dataset": {
         "dataset_dir": args.dataset_dir,
+        "backbone_dataset_size": args.backbone_dataset_size,
+        "backbone_val_size": args.backbone_val_size,
     },
+
     "backbone": {
         "type": args.backbone_type,
         "input_size": args.backbone_input_size,
     },
-    "trainer": {},
+
+    "trainer": {
+        "backbone_n_epochs": args.backbone_n_epochs,
+    },
+
     "tester": {},
     "metrics": {}
 }
